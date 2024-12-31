52c7f7b77039612638d3c43d19ce8b223e94c3f5
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2024-12-31 13:41:23 -0300

    habilitanto los cors y el logging de morgan

---
+++ backend/src/config/cors.ts
@@ -0,0 +1,9 @@
+import cors, { CorsOptions } from 'cors';
+
+export const corsOptions: CorsOptions = {
+  origin: function(origin, callback) {
+    const whiteList = [process.env.FRONTEND_URL]
+    if (whiteList.includes(origin)) callback(null, true)
+    else callback(new Error("Error de CORS"))
+  }
+}
