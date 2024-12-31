9951a1214e9b6359fc65b71ccfe5e78cd2e2d767
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2024-12-31 17:53:22 -0300

    borrando

--- backend/src/routes/productRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { ProductController } from "../controllers/productController";
-
-const router = Router()
-
-router.post('/', ProductController.createProduct)
-router.get('/', ProductController.getAllProducts)
-
-export default router
--- frontend/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export const formatCurrency = (value: number) => {
-  return value.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' });
-}
