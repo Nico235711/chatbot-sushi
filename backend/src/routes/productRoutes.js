8de1d3c036655b39f4418dd4e34c63295854a937
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-01-02 18:24:09 -0300

    añadiendo un middleware

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
 import { ProductController } from "../controllers/productController.js";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/index.js";
 
 const router = Router();
 
-router.post("/", ProductController.createProduct);
+router.post("/", 
+  body("name").notEmpty().withMessage("El nombre del producto es obligatorio"),
+  body("description").notEmpty().withMessage("La descripcion del producto es obligatoria"),
+  body("price")
+    .isNumeric().withMessage("El precio del producto debe ser un numero")
+    .custom(price => price > 0).withMessage("El precio del producto debe ser mayor a cero"),
+  handleInputErrors,
+  ProductController.createProduct
+);
+
 router.get("/", ProductController.getAllProducts);
 
 export default router;
